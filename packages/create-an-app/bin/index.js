37436dc44aecb884e4591bf050e2d0563f855cc1
Haja Rasoahaingo <haja.rasoah@gmail.com>
2018-02-25 19:13:50 +0100

    update

--- packages/create-an-app/bin/index.js
+++ packages/create-an-app/bin/index.js
@@ -7,8 +7,9 @@ const currentNodeVersion = process.versions.node;
 const [major] = currentNodeVersion.split('.');
 
 if (major < 8) {
-  console.log(`node v${colors.error('error')} ${currentNodeVersion} is not compatible.`);
-  console.log(`Please upgrade to node ${engines.node}`);
+  console.log(`${colors.error('error')} Your node v${currentNodeVersion} is not compatible.`);
+  console.log(`Please upgrade to node ${engines.node} (LTS version) for better performance.`);
+  console.log('Fore more informations please read http://bit.ly/2h4yccC');
   process.exit(1);
 }
 
