1619b525805ed91391cf8d8ad5afc9c51805c6b0
Haja Rasoahaingo <haja.rasoah@gmail.com>
2018-03-02 02:15:00 +0100

    update

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -4,12 +4,12 @@ const through = require('through');
 const { execSync } = require('child_process');
 
 const tag = process.argv[2] ? `v${process.argv[2]}` : 'next';
-const tagName = `v${process.argv[3]}`;
+const tagName = process.argv[3] ? `v${process.argv[3]}` : null;
 const md = path.resolve(__dirname, '../CHANGELOG.md');
 const tmp = path.resolve(__dirname, '../CHANGELOG.tmp.md');
 const wr = fs.createWriteStream(tmp);
-const rr = fs.createReadStream(md, { encoding: 'utf8' });
-rr
+
+fs.createReadStream(md, { encoding: 'utf8' })
   .pipe(through(
     function write(data) {
       const headlines = /## ([0-9a-z.]+)/;
@@ -19,7 +19,7 @@ rr
           const result = execSync(`lerna-changelog --tag-from ${match[1]} --tag-to ${tag}`);
           const changelog = Buffer.from(result).toString().trim();
           if (changelog) {
-            this.queue(changelog.replace(tag, tagName));
+            this.queue(changelog.replace(tag, tagName || tag));
             this.queue('\n\n');
           }
         } catch (err) {
