2b4eabf618bf5c58b54e1a3269f8a82cb2ed8f4c
Haja Rasoahaingo <haja.rasoah@gmail.com>
2018-03-02 01:44:01 +0100

    add changelog generator script (#8)

---
+++ CHANGELOG.md
@@ -0,0 +1,31 @@
+## v0.0.21 (2018-03-01)
+
+#### :rocket: New Feature
+* `a-react-template`
+  * [#7](https://github.com/hrasoa/create-an-app/pull/7) update changelog. ([@hrasoa](https://github.com/hrasoa))
+
+#### Committers: 1
+- Haja Rasoahaingo ([hrasoa](https://github.com/hrasoa))
+## v0.0.20 (2018-02-28)
+
+#### :rocket: New Feature
+* `create-an-app`
+  * [#4](https://github.com/hrasoa/create-an-app/pull/4) update prompt create message. ([@hrasoa](https://github.com/hrasoa))
+
+#### :nail_care: Polish
+* [#2](https://github.com/hrasoa/create-an-app/pull/2) remove allowed branches. ([@hrasoa](https://github.com/hrasoa))
+
+#### Committers: 1
+- Haja Rasoahaingo ([hrasoa](https://github.com/hrasoa))
+
+## v0.0.2 (2018-02-28)
+
+#### :rocket: New Feature
+* `create-an-app`
+  * [#4](https://github.com/hrasoa/create-an-app/pull/4) update prompt create message. ([@hrasoa](https://github.com/hrasoa))
+
+#### :nail_care: Polish
+* [#2](https://github.com/hrasoa/create-an-app/pull/2) remove allowed branches. ([@hrasoa](https://github.com/hrasoa))
+
+#### Committers: 1
+- Haja Rasoahaingo ([hrasoa](https://github.com/hrasoa))
--- scripts/changelog.js
+++ scripts/changelog.js
@@ -1,21 +1,41 @@
 const fs = require('fs');
 const path = require('path');
-const spawn = require('cross-spawn');
-const { version } = require('../lerna.json');
+const through = require('through');
+const { execSync } = require('child_process');
 
-const rr = fs.createReadStream(path.resolve(__dirname, '../CHANGELOG.md'), {
-  start: 4,
-  end: 8,
-  encoding: 'utf8',
-});
-rr.on('readable', () => {
-  const lastVersion = rr.read();
-  spawn('lerna-changelog', [
-    '--tag-from',
-    lastVersion,
-    '--tag-to',
-    version,
-  ], {
-    stdio: 'inherit',
-  });
+// create a rc tag
+// create a changelog to rc tag with final tag name
+const tag = process.argv[2] || 'next';
+const tagName = process.argv[3];
+const md = path.resolve(__dirname, '../CHANGELOG.md');
+const tmp = path.resolve(__dirname, '../CHANGELOG.tmp.md');
+const wr = fs.createWriteStream(tmp);
+const rr = fs.createReadStream(md, { encoding: 'utf8' });
+rr
+  .pipe(through(
+    function write(data) {
+      const headlines = /## ([0-9a-z.]+)/;
+      const match = headlines.exec(data);
+      if (match && match.length >= 2) {
+        try {
+          const result = execSync(`lerna-changelog --tag-from ${match[1]} --tag-to ${tag}`);
+          const changelog = Buffer.from(result).toString().trim();
+          if (changelog) {
+            this.queue(changelog.replace(/unreleased/i, tagName));
+            this.queue('\n');
+          }
+        } catch (err) {
+          this.queue(null);
+        }
+      }
+      this.queue(data.trim());
+    },
+    function end() {
+      this.queue(null);
+    },
+  ))
+  .pipe(wr);
+
+fs.rename(tmp, md, (err) => {
+  if (err) console.log(err);
 });
